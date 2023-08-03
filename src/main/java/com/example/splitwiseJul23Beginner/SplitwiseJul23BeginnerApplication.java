4cf0d21e221fcc90f54ec97b8be0e0dc9f5074db
Bhavik Dand <bhavikdand@Bhaviks-MacBook-Pro-3.local>
2023-08-03 07:21:01 +0530

    Porject init

---
+++ src/main/java/com/example/splitwiseJul23Beginner/SplitwiseJul23BeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseJul23Beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseJul23BeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseJul23BeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/splitwiseJul23Beginner/SplitwiseJul23BeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseJul23Beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseJul23BeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
